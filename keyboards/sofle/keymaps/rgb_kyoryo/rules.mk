bc58a87818117d46f60a79923dd2999c40b80cf0
Kyoryo Ishigami <franz.rio19@gmail.com>
2024-05-10 22:10:57 +0700

    update docs

--- keyboards/sofle/keymaps/rgb_kyoryo/rules.mk
+++ keyboards/sofle/keymaps/rgb_kyoryo/rules.mk
@@ -1,8 +1,17 @@
+# enable mouse support
 MOUSEKEY_ENABLE = yes
+# enable audio and multimedia keys
 EXTRAKEY_ENABLE = yes
+# enable console for debugging
 CONSOLE_ENABLE = no
+# enable underglow
 RGBLIGHT_ENABLE = yes
+# enable knob
 ENCODER_ENABLE = yes
+# reduce size, increase build time
 LTO_ENABLE = yes
+# enable oled display
 OLED_ENABLE = yes
-VIA_ENABLED = yes
+# enable via support, only 4 layers applicable
+VIA_ENABLED = no
+
